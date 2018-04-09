11418f6fe8a36a745699ed67ab33adfa88b4e09f
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-09 12:46:23 +0200

    Update toponimos_install.sh

--- toponimos_install.sh
+++ toponimos_install.sh
@@ -5,3 +5,7 @@ sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spai
 
 # 2.Acceso directo
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons
+
+# Repositorio: partner-contact
+sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact
+sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/custom/addons
