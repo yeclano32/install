693c1eeb857ebeee14b627f1542df6994ac2a793
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-10 11:52:52 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -2,6 +2,11 @@
 
 # 1.Descarga
 sudo git clone https://github.com/OCA/account-financial-reporting.git -b 10.0 /opt/odoo/account-financial-reporting
+sudo git clone https://github.com/OCA/account-financial-tools.git -b 10.0 /opt/odoo/account-financial-tools
+
+
 
 # 2.Acceso directo
 sudo ln -s /opt/odoo/account-financial-reporting/account_export_csv /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-reporting/account_move_line_report_xls /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons
