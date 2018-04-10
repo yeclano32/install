876f2acc3deb0c00dcaf0b648924b38cd5bcc1b9
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-10 12:34:46 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -3,10 +3,20 @@
 # 1.Descarga
 sudo git clone https://github.com/OCA/account-financial-reporting.git -b 10.0 /opt/odoo/account-financial-reporting
 sudo git clone https://github.com/OCA/account-financial-tools.git -b 10.0 /opt/odoo/account-financial-tools
-
-
+sudo git clone https://github.com/OCA/account-payment.git -b 10.0 /opt/odoo/account-payment
+bank-payment
 
 # 2.Acceso directo
 sudo ln -s /opt/odoo/account-financial-reporting/account_export_csv /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-reporting/account_move_line_report_xls /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_journal_always_check_date /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_renumber /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-payment/account_due_list /opt/odoo/custom/addons
+account-payment/account_due_list_payment_mode
+bank-payment/account_banking_mandate
+bank-payment/account_banking_pain_base
+bank-payment/account_banking_sepa_direct_debit
+bank-payment/account_payment_partner
