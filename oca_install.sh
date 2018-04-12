2c61f4eef47505c9f1b3ccde9d93f50d436f8ffa
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-12 12:45:35 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -14,34 +14,33 @@ sudo git clone https://github.com/OCA/web.git -b 10.0 /opt/odoo/web
 
 # 2.Acceso directo
 
-
+# Repositorio: account-financial-tools
 sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
-
 sudo ln -s /opt/odoo/account-financial-tools/account_renumber /opt/odoo/custom/addons
+
+# Repositorio: account-payment
 sudo ln -s /opt/odoo/account-payment/account_due_list /opt/odoo/custom/addons
-sudo ln -s /opt/odoo/account-payment/account_due_list_payment_mode /opt/odoo/custom/addons 
+sudo ln -s /opt/odoo/account-payment/account_due_list_payment_mode /opt/odoo/custom/addons
+
+# Repositorio: bank-payment
 sudo ln -s /opt/odoo/bank-payment/account_banking_mandate /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/bank-payment/account_banking_pain_base /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/bank-payment/account_banking_sepa_direct_debit /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/bank-payment/account_payment_partner /opt/odoo/custom/addons
 
-
+# Repositorio: knowledge
 sudo ln -s /opt/odoo/knowledge/document_page /opt/odoo/custom/addons
 
-
-
+# Repositorio: l10n-spain
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_asset /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_balance_report /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_bank_statement_import_n43 /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_invoice_sequence /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod111 /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod115 /opt/odoo/custom/addons
-
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod303 /opt/odoo/custom/addons
-
-
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod349 /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_fiscal_year_closing /opt/odoo/custom/addons
@@ -50,12 +49,19 @@ sudo ln -s /opt/odoo/l10n-spain/account_balance_reporting_xlsx /opt/odoo/custom/
 sudo ln -s /opt/odoo/l10n-spain/l10n_es /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons
 
+# Repositorio: partner-contact
 sudo ln -s /opt/odoo/partner-contact/base_location /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/partner-contact/base_partner_sequence /opt/odoo/custom/addons
+
+# Repositorio: reporting-engine
 sudo ln -s /opt/odoo/reporting-engine/report_xlsx /opt/odoo/custom/addons
+
+# Repositorio: server-tools
 sudo ln -s /opt/odoo/server-tools/disable_odoo_online /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/server-tools/mass_editing /opt/odoo/custom/addons
+
+# Repositorio: web
 sudo ln -s /opt/odoo/web/web_export_view /opt/odoo/custom/addons
 
 
