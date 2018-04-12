2bed4239b300bfae6882371b21a20cf4ee24f282
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-12 19:18:06 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -58,6 +58,7 @@ sudo ln -s /opt/odoo/reporting-engine/report_xlsx /opt/odoo/custom/addons
 # Repositorio: server-tools
 sudo ln -s /opt/odoo/server-tools/disable_odoo_online /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/server-tools/mass_editing /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/date_range /opt/odoo/custom/addons
 
 # Repositorio: web
 sudo ln -s /opt/odoo/web/web_export_view /opt/odoo/custom/addons
