951ebad5c3e757b0285d512d6ba3eb2857ae13dc
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-26 21:34:57 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -11,11 +11,11 @@ sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/part
 sudo git clone https://github.com/OCA/reporting-engine.git -b 10.0 /opt/odoo/reporting-engine
 sudo git clone https://github.com/OCA/server-tools.git -b 10.0 /opt/odoo/server-tools
 sudo git clone https://github.com/OCA/web.git -b 10.0 /opt/odoo/web
-
+sudo git clone https://github.com/OCA/account-closing-b 10.0 /opt/odoo/account-closing
 
 # 2.Acceso directo
 
-
+sudo ln -s /opt/odoo/account-closing/account_fiscal_year_closing /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
