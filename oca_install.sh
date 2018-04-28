7047ebbb0ada30c4694842c73d6e20a2c40d78c8
yeclano32 <38205540+yeclano32@users.noreply.github.com>
2018-04-28 19:22:43 +0200

    Update oca_install.sh

--- oca_install.sh
+++ oca_install.sh
@@ -55,6 +55,7 @@ sudo ln -s /opt/odoo/partner-contact/base_location /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/partner-contact/base_partner_sequence /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/reporting-engine/report_xlsx /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/date_range /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/server-tools/disable_odoo_online /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/server-tools/mass_editing /opt/odoo/custom/addons
 sudo ln -s /opt/odoo/web/web_export_view /opt/odoo/custom/addons
